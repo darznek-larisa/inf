f31089d86d3adc8d783f7f86cd3b01974f8d7cde
darznek-larisa <79532497+darznek-larisa@users.noreply.github.com>
2021-06-01 12:34:26 +0300

    Lab 5

---
+++ Tree/ExpressionNode.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace Tree
+{
+    class ExpressionNode
+    {
+        public string value { get; set; }
+        public ExpressionNode  left { get; set; }
+        public ExpressionNode right { get; set; }
+        public string exp { get; set; }
+        public ExpressionNode(string c)
+        {
+            if ((c.Length == 1 && isOperator(c[0])) || double.TryParse(c, out double number))
+            {
+                exp = null;
+                this.value = c;
+                left = null;
+                right = null;
+            }
+            else
+            {
+                Console.WriteLine("В дерево поступило не числовое значение и не оператор!");
+                System.Environment.Exit(0);
+            }
+
+            }
+        bool isOperator(char c)
+        {
+            if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^')
+            {
+                return true;
+            }
+            return false;
+        }
+        public double CalcTree (ExpressionNode tree)
+        {
+            double num1, num2;
+            if (tree.left == null)
+            {
+                return Convert.ToDouble(new DataTable().Compute(tree.value, ""));
+            }
+            num1 = CalcTree(tree.left);
+            num2 = CalcTree(tree.right);
+            switch (tree.value)
+            {
+                case "+":
+                    return num1 + num2;
+                case "-":
+                    return num1 - num2;
+                case "*":
+                    return num1 * num2;
+                case "/":
+                    return num1 / num2;
+            }
+            return 34567;
+        }
+    }
+}
---
+++ Tree/TreeNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    class TreeNode
+    {
+        public int value { get; set; }
+        public TreeNode left { get; set; }
+        public TreeNode right { get; set; }
+        public TreeNode(int value)
+        {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+}
